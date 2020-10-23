413f2d4c907c3573b822354976caa21b1c11c19c
Mike Colbert <michael-colbert@uiowa.edu>
2020-10-23 14:21:21 -0500

    data from form is being populated into python variables

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/mnthlyPmt', methods=['POST'])
+def mnthlyPmt():
+    if request.method == 'POST':
+        form = request.form
+        loanAmt = float(form['loanAmt'])
+        lengthOfLoan= float(form['lengthOfLoan'])
+        intRate= float(form['intRate'])
+
+        mySum = loanAmt + lengthOfLoan + intRate
+
+        print("******************")
+        print(loanAmt)
+        print(lengthOfLoan)
+        print(intRate)
+        print (mySum)
+        #return render_template('index.html', display=calc, pageTitle='My Calculator')
+        
+    return render_template('monthlyPayment.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
