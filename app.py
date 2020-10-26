75d075709ea81fddd600e4baa55f33766b8fe98b
Mike Colbert <michael-colbert@uiowa.edu>
2020-10-26 14:07:12 -0500

    loan calculation is working correctly

--- app.py
+++ app.py
@@ -3,6 +3,30 @@ from flask import render_template, request
 
 app = Flask(__name__)
 
+class Loan:
+    def __init__(self, loanAmount, numberYears, annualRate):
+        self.loanAmount = loanAmount
+        self.annualRate = annualRate
+        self.numberOfPmts = numberYears * 12 #monthly pmts
+        self.periodicIntRate = self.annualRate / 12
+        self.discountFactor = 0.0
+        self.loanPmt = 0
+        
+    def getDiscountFactor(self):
+        return self.discountFactor
+    
+    def calculateDiscountFactor(self):
+        self.discountFactor = (((1.0 + self.periodicIntRate) ** self.numberOfPmts) - 1.0) / (self.periodicIntRate * (1.0 + self.periodicIntRate) ** self.numberOfPmts)
+        
+    def calculateLoanPmt(self):
+        self.calculateDiscountFactor()
+        self.loanPmt = self.loanAmount / self.getDiscountFactor()
+        
+    def getLoanPmt(self):
+        return self.loanPmt
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,19 +36,20 @@ def mnthlyPmt():
     if request.method == 'POST':
         form = request.form
         loanAmt = float(form['loanAmt'])
-        lengthOfLoan= float(form['lengthOfLoan'])
-        intRate= float(form['intRate'])
+        numberYears= float(form['lengthOfLoan'])
+        annualRate= float(form['intRate'])
 
-        mySum = loanAmt + lengthOfLoan + intRate
+        loan = Loan(loanAmt, numberYears, annualRate)
 
-        print("******************")
-        print(loanAmt)
-        print(lengthOfLoan)
-        print(intRate)
-        print (mySum)
-        #return render_template('index.html', display=calc, pageTitle='My Calculator')
+        loan.calculateLoanPmt()
+
+        mnthlyLoanPmt = loan.getLoanPmt()
+
+        print(mnthlyLoanPmt)
+
+        return render_template('index.html', mnthlyPmt = mnthlyLoanPmt)
         
-    return render_template('monthlyPayment.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
